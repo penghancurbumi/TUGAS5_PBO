ad731d32efcdabb7478a0835ce51574c0f82365d
penghancurbumi <161696524+penghancurbumi@users.noreply.github.com>
2025-05-05 21:41:12 +0700

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,41 @@
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String jurusan;
+
+    public Mahasiswa(String nim, String nama, String jurusan) {
+        this.nim = nim;
+        this.nama = nama;
+        this.jurusan = jurusan;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public void displayInfo() {
+        System.out.println("NIM: " + nim);
+        System.out.println("Nama: " + nama);
+        System.out.println("Jurusan: " + jurusan);
+    }
+}
