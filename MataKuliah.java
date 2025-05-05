dcfa609ca0953b6f5b71b9f1d881800fc6ee0002
penghancurbumi <161696524+penghancurbumi@users.noreply.github.com>
2025-05-05 21:39:39 +0700

    Create MataKuliah.java

---
+++ MataKuliah.java
@@ -0,0 +1,41 @@
+public class MataKuliah {
+    private String kode;
+    private String nama;
+    private int sks;
+
+    public MataKuliah(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    public void setSks(int sks) {
+        this.sks = sks;
+    }
+
+    public void displayInfo() {
+        System.out.println("Kode: " + kode);
+        System.out.println("Mata Kuliah: " + nama);
+        System.out.println("SKS: " + sks);
+    }
+}
